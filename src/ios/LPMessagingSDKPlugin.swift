afac9f5c2975c94b0410f621a35aaa97086c7eae
Gonçalo Costa Ferraria <gferraria@gmail.com>
2020-03-15 11:42:56 +0000

    Update LPMessagingSDKPlugin.swift

--- src/ios/LPMessagingSDKPlugin.swift
+++ src/ios/LPMessagingSDKPlugin.swift
@@ -30,26 +30,4 @@ extension String {
 
 @objc(LPMessagingSDKPlugin) class LPMessagingSDKPlugin: CDVPlugin {
 
-	var conversationQuery: ConversationParamProtocol?
-
-    // adding delegates and callbacks for Cordova to notify javascript wrapper when functions complete
-    var callBackCommandDelegate: CDVCommandDelegate?
-    var callBackCommand:CDVInvokedUrlCommand?
-  
-    var registerLpPusherCallbackCommandDelegate: CDVCommandDelegate?
-    var registerLpPusherCallbackCommand: CDVInvokedUrlCommand?
-
-    var globalCallbackCommandDelegate: CDVCommandDelegate?
-    var globalCallbackCommand: CDVInvokedUrlCommand?
-
-    var lpAccountNumber:String?
-
-    override init() {
-        super.init()
-    }
-
-    override func pluginInitialize() {
-        print("@@@ iOS pluginInitialize")
-    }
-
 }
