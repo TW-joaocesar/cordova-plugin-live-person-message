f8e10554002615e51c6e78e5173a33d50afa6064
Gonçalo Costa Ferraria <gferraria@gmail.com>
2020-03-17 15:22:22 +0000

    Update LPMessagingSDKPlugin.swift

--- src/ios/LPMessagingSDKPlugin.swift
+++ src/ios/LPMessagingSDKPlugin.swift
@@ -25,6 +25,55 @@ import Foundation
         print("@@@ iOS pluginInitialize")
     }
 
+	@objc(lp_sdk_init:)
+    func lp_sdk_init(command: CDVInvokedUrlCommand) {
+
+		guard let lpAccountNumber = command.arguments.first as? String else {
+            print("Can't init without brandID")
+            return
+        }
+
+        self.lpAccountNumber = lpAccountNumber
+
+        print("lpMessagingSdkInit brandID --> \(lpAccountNumber)")
+
+	}
+
+	func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+
+    func convertDeviceTokenString(token:String) -> Data {
+        
+        var result: [String] = []
+        let characters = Array(token)
+        stride(from: 0, to: characters.count, by: 8).forEach {
+            result.append(String(characters[$0..<min($0+8, characters.count)]))
+        }
+
+        var tokenAsString = result.joined(separator: " ")
+
+        tokenAsString = "<" + tokenAsString + ">"
+
+        let tokenAsNSData = tokenAsString.hexadecimal()! as NSData
+        let tokenAsData = tokenAsString.hexadecimal()!
+
+        print("@@@ tokenAsNSData \(tokenAsNSData)")
+        print("@@@ tokenAsData \(tokenAsData)")
+
+        print("@@@ string as 8 character chunks ... \(result)")
+        print("@@@ tokenAsString --> \(tokenAsString)" )
+
+        return tokenAsData
+    }
+
 }
 
 extension String {
